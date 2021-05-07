d5051e43407c420d4cd974013eaee88a16be603b
asoilihi <abdoulhalim.soilihi@outlook.fr>
2021-05-07 11:38:34 +0200

    aso - ajout des recherches en BDD pour les modules legacy et new

--- core-legacy/src/main/java/com/example/demo/core/ShoeCoreLegacy.java
+++ core-legacy/src/main/java/com/example/demo/core/ShoeCoreLegacy.java
@@ -1,23 +1,38 @@
 package com.example.demo.core;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.demo.dto.in.ShoeFilter;
 import com.example.demo.dto.in.ShoeFilter.Color;
 import com.example.demo.dto.out.Shoe;
 import com.example.demo.dto.out.Shoes;
-import java.math.BigInteger;
-import java.util.List;
+import com.example.demo.entities.ShoeEntity;
+import com.example.demo.mapper.StockMapper;
+import com.example.demo.repositories.ShoeRepository;
 
 @Implementation(version = 1)
 public class ShoeCoreLegacy extends AbstractShoeCore {
 
-  @Override
-  public Shoes search(final ShoeFilter filter) {
-    return Shoes.builder()
-                .shoes(List.of(Shoe.builder()
-                                   .name("Legacy shoe")
-                                   .color(Color.BLUE)
-                                   .size(BigInteger.ONE)
-                                   .build()))
-                .build();
-  }
+	@Autowired
+	private ShoeRepository shoeRepository;
+
+	@Autowired
+	private StockMapper stockMapper;
+
+	@Override
+	public Shoes search(final ShoeFilter filter) {
+
+		List<Shoe> listShoes = new ArrayList<Shoe>();
+		listShoes.add(Shoe.builder().name("Legacy shoe").color(Color.BLUE).size(BigInteger.ONE).build());
+
+		List<ShoeEntity> listEntityShoes = shoeRepository.findAll(prepareExampleMatcherShoeToSearch(filter));
+
+		listEntityShoes.forEach(shoeEntity -> listShoes.add(stockMapper.shoeEntityToShoe(shoeEntity)));
+
+		return Shoes.builder().shoes(listShoes).build();
+	}
 }
--- core-new/src/main/java/com/example/demo/core/ShoeCoreNew.java
+++ core-new/src/main/java/com/example/demo/core/ShoeCoreNew.java
@@ -1,23 +1,39 @@
 package com.example.demo.core;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.example.demo.dto.in.ShoeFilter;
 import com.example.demo.dto.in.ShoeFilter.Color;
 import com.example.demo.dto.out.Shoe;
 import com.example.demo.dto.out.Shoes;
-import java.math.BigInteger;
-import java.util.List;
+import com.example.demo.entities.ShoeEntity;
+import com.example.demo.mapper.StockMapper;
+import com.example.demo.repositories.ShoeRepository;
 
 @Implementation(version = 2)
 public class ShoeCoreNew extends AbstractShoeCore {
 
-  @Override
-  public Shoes search(final ShoeFilter filter) {
-    return Shoes.builder()
-                .shoes(List.of(Shoe.builder()
-                                   .name("New shoe")
-                                   .color(filter.getColor().orElse(Color.BLACK))
-                                   .size(filter.getSize().orElse(BigInteger.TWO))
-                                   .build()))
-                .build();
-  }
+	@Autowired
+	private ShoeRepository shoeRepository;
+
+	@Autowired
+	private StockMapper stockMapper;
+
+	@Override
+	public Shoes search(final ShoeFilter filter) {
+		List<Shoe> listShoes = new ArrayList<Shoe>();
+		listShoes.add(Shoe.builder().name("New shoe").color(filter.getColor().orElse(Color.BLACK))
+				.size(filter.getSize().orElse(BigInteger.TWO)).build());
+
+		List<ShoeEntity> listEntityShoes = shoeRepository.findAll(prepareExampleMatcherShoeToSearch(filter));
+
+		listEntityShoes.forEach(shoeEntity -> listShoes.add(stockMapper.shoeEntityToShoe(shoeEntity)));
+
+		return Shoes.builder().shoes(listShoes).build();
+	}
+
 }
